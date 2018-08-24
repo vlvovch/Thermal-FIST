31d44dd9e82d59ccbac234a8c432ecac4683b019
Volodymyr Vovchenko <vlvovch@gmail.com>
2018-08-24 14:36:19 +0200

    Mass-dependent momentum scale in MC generator

--- src/libraries/HRGBase/ThermalParticle.cpp
+++ src/libraries/HRGBase/ThermalParticle.cpp
@@ -15,7 +15,9 @@ using namespace std;
 double ParticleDecay::ModifiedWidth(double m)
 {
 	if (m < mM0) return 0.;
-	if (mM0 >= mPole) return mBratio;
+	//if (mM0 >= mPole) return mBratio;
+	if (mM0 >= mPole)
+		return mBratio * pow(1. - (mM0 / m)*(mM0 / m), mL + 1. / 2.);
   return mBratio * pow(1. - (mM0 / m)*(mM0 / m), mL + 1. / 2.) / pow(1. - (mM0 / mPole)*(mM0 / mPole), mL + 1. / 2.);
 	//return mBratio * pow((m - mM0) / (mPole - mM0), 1. / 2.); // as in 1808.02106
 }
