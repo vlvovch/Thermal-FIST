b52fc647edbdcd196344f86303c16a7f747c8316
Volodymyr Vovchenko <vlvovch@gmail.com>
2025-07-21 18:38:41 -0700

    Implemented the rest of m->0 cluster expansion functions and added checks for zero width mode in event generator

--- src/library/HRGEventGenerator/EventGeneratorBase.cpp
+++ src/library/HRGEventGenerator/EventGeneratorBase.cpp
@@ -1251,7 +1251,11 @@ namespace thermalfist {
     bool flOverlap = true;
     while (flOverlap) {
       int sampled = 0;
-      if(ids.size() == 0) break;
+      
+      // Check if the event has no particles at all
+      if(ids.size() == 0) 
+        break;
+      
       while (sampled < ids.size()) {
         flOverlap = false;
         int i = ids[sampled];
@@ -1518,7 +1522,7 @@ namespace thermalfist {
                     }
                   }
 
-                  // Sample lifetime in rest frame
+                  // Sample the lifetime in the rest frame
                   ct = -ct * log(1. - RandomGenerators::randgenMT.randDblExc());
 
                   // Lorentz time delay
@@ -1565,7 +1569,7 @@ namespace thermalfist {
                 primParticles[i][j].processed = true;
               }
               else {
-                // Decay through unknown branching ratio, presumably radiative, no hadrons, just ignore decay products
+                // Decay through unknown branching ratio, presumably radiative, no hadrons, just ignore the decay products
                 primParticles[i][j].processed = true;
               }
             }
